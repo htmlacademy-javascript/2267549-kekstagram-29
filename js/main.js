8516d6cd74dfc8378616eadc2c24536385f757ab
Ksenia <malyshkinakb@gmail.com>
2023-07-20 23:51:11 +0300

    Добавляет исправления

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 import {createPhotos} from './data.js';
-//import {insertElementsToPage} from './photos.js';
-//import './popup-photo.js';
 import {renderGallery} from './gallery.js';
 
 renderGallery(createPhotos());
-
-
-
